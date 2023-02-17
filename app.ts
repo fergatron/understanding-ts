31923210e28dd50d2f0815bc5ee2ab374b3d5ce1
Marc Ferguson <marc@fergytech.com>
2023-02-16 20:34:58 -0500

    Introduced tuple data type.

--- app.ts
+++ app.ts
@@ -1,8 +1,20 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];        // array
+    role: [number, string]    // tuple
+} = {
     name: 'Marc',
     age: 43,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
+
+person.role.push(444);
+// person.role[1] = 10;
+person.role = [178, 'what'];
+
+console.log(person);
 
 let favoriteActivites: string[];
 favoriteActivites = ['Sport'];
