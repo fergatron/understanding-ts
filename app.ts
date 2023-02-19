e49826f8d7f5b7197e19c8dd49f5aa6feec216a7
Marc Ferguson <marc@fergytech.com>
2023-02-19 16:35:45 -0500

    Learned union types and custom types.

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
-function combine(input1: number | string, input2: number | string) {
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        return input1 + input2;
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion: ConversionDescriptor
+    ) {
+    let result;
+    if (
+        typeof input1 === 'number'
+        && typeof input2 === 'number'
+        || resultConversion === 'as-number'
+    ) {
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
     }
-    return input1.toString() + input2.toString();
+    if (resultConversion === 'as-number') {
+        return +result;
+    } else {
+        return result.toString();
+    }
 }
 
-console.log('numbers', combine(30, 26));
-console.log('strings', combine('Max', 'Anna'));
+console.log('numbers', combine(30, 26, 'as-number'));
+console.log('strings', combine('Max', 'Anna', 'as-text'));
+console.log('combined strings', combine('36', '34', 'as-number'));
